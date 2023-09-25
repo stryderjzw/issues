72147469ffab58482b4bc92abb5851fdebf90c90
Justin Wong <stryderjzw@gmail.com>
2023-09-24 22:41:38 -0700

    add table_formatter

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -1,4 +1,6 @@
 defmodule Issues.CLI do
+  import Issues.TableFormatter, only: [print_table_for_columns: 2]
+
   @default_count 4
 
   @moduledoc """
@@ -24,6 +26,7 @@ defmodule Issues.CLI do
     |> decode_response()
     |> sort_into_descending_order()
     |> last(count)
+    |> print_table_for_columns(["number", "title", "created_at"])
   end
 
   def decode_response({:ok, body}), do: body
